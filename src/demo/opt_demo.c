adf50b751ac5b737095cb5751358eb4bc24aa7a3
huangyongtao <huangyongtao@hangsheng.com.cn>
2020-12-28 14:39:10 +0800

    匹配opt 长短指令

--- src/demo/opt_demo.c
+++ src/demo/opt_demo.c
@@ -34,32 +34,32 @@
 //     (3)一个字符，后接两个冒号——表示选项后面带一个可选参数，即参数可有可无， 如果带参数，则选项与参数直接不能有空格
 
 // 全局变量：
-//     1.optarg：表示当前选项对应的参数值。
-//     2.optind：表示的是下一个将被处理到的参数在argv中的下标值。
-//     3.opterr：如果opterr = 0，在getopt、getopt_long、getopt_long_only遇到错误将不会输出错误信息到标准输出流。opterr在非0时，向屏幕输出错误。
-//     4.optopt：表示没有被未标识的选项。
+//     (1)optarg：表示当前选项对应的参数值。
+//     (2)optind：表示的是下一个将被处理到的参数在argv中的下标值。
+//     (3)opterr：如果opterr = 0，在getopt、getopt_long、getopt_long_only遇到错误将不会输出错误信息到标准输出流。opterr在非0时，向屏幕输出错误。
+//     (4)optopt：表示没有被未标识的选项。
 
 
 // 返回值：
-//     1.如果短选项找到，那么将返回短选项对应的字符。
-//     2.如果长选项找到，如果flag为NULL，返回val。如果flag不为空，返回0
-//     3.如果遇到一个选项没有在短字符、长字符里面。或者在长字符里面存在二义性的，返回“？”
-//     4.如果解析完所有字符没有找到（一般是输入命令参数格式错误，eg： 连斜杠都没有加的选项），返回“-1”
-//     5.如果选项需要参数，忘了添加参数。返回值取决于optstring，如果其第一个字符是“：”，则返回“：”，否则返回“？”。
+//     (1)如果短选项找到，那么将返回短选项对应的字符。
+//     (2)如果长选项找到，如果flag为NULL，返回val。如果flag不为空，返回0
+//     (3)如果遇到一个选项没有在短字符、长字符里面。或者在长字符里面存在二义性的，返回“？”
+//     (4)如果解析完所有字符没有找到（一般是输入命令参数格式错误，eg： 连斜杠都没有加的选项），返回“-1”
+//     (5)如果选项需要参数，忘了添加参数。返回值取决于optstring，如果其第一个字符是“：”，则返回“：”，否则返回“？”。
 
 // 注意：
-//     1.longopts的最后一个元素必须是全0填充，否则会报段错误
-//     2.短选项中每个选项都是唯一的。而长选项如果简写，也需要保持唯一性。
+//     (1)longopts的最后一个元素必须是全0填充，否则会报段错误
+//     (2)短选项中每个选项都是唯一的。而长选项如果简写，也需要保持唯一性。
 
-static const char *g_shortopts = "a:id:vc::f:";
+static const char* g_shortopts = "a:id:vc::f:";
 static struct option g_long_options[] = {
-            {"add",     required_argument, 0,  0 },
-            {"append",  no_argument,       0,  0 },
-            {"delete",  required_argument, 0,  0 },
-            {"verbose", no_argument,       0,  0 },
-            {"create",  required_argument, 0, 'c'},
-            {"file",    required_argument, 0,  0 },
-            {0,         0,                 0,  0 }      /*end*/
+            {"add",     required_argument, 0,  'i' },
+            {"append",  no_argument,       0,  'a' },
+            {"delete",  required_argument, 0,  'd' },
+            {"verbose", no_argument,       0,  'v' },
+            {"create",  required_argument, 0,  'c' },
+            {"file",    required_argument, 0,  'f' },
+            {0,         0,                 0,  0   }      /*end*/
 };
 
 
